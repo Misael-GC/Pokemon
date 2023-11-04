91f1db8a2efca42d228f0ac704b597c70753cce3
Misael-GC <misael.gomez1623@gmail.com>
2023-11-04 13:32:32 -0600

    Ataque en JS - lógica inicial para los botones  de los diferentes ataques, definir funciones y DOM de los respectivos selectores

--- src/mokempon/js/index.js
+++ src/mokempon/js/index.js
@@ -1,6 +1,15 @@
+let ataqueJugador;
+
 function  iniciarJuego(){
     let botonMascotaJugador = document.getElementById('boton-mascota');
     botonMascotaJugador.addEventListener('click', seleccionarMascotaJugador);
+
+    let botonFuego = document.getElementById('boton-fuego')
+    botonFuego.addEventListener('click', ataqueFuego)
+    let botonAgua = document.getElementById('boton-agua')
+    botonAgua.addEventListener('click', ataqueAgua)
+    let botonTierra = document.getElementById('boton-tierra')
+    botonTierra.addEventListener('click', ataqueTierra)
 };
 
 function seleccionarMascotaJugador(){
@@ -34,7 +43,18 @@ function seleccionarMascotaEnemigo(){
 
 }
 
-
+function ataqueFuego(){
+    ataqueJugador = 'Fuego'
+    alert(ataqueJugador)
+}
+function ataqueAgua(){
+    ataqueJugador = 'Agua';
+    alert(ataqueJugador);
+}
+function ataqueTierra(){
+    ataqueJugador = 'Tierra'
+    alert(ataqueJugador);
+}
 
 function aleatorio(min, max) {
     return Math.floor(Math.random() * (max - min + 1) + min)
